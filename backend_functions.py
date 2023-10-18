7db4304d9cfc8f45460d795835a0d22a8317b788
vijaya <vijaya.mnair318@gmail.com>
2023-10-18 23:45:36 +0300

    Task 3

--- backend_functions.py
+++ backend_functions.py
@@ -6,8 +6,9 @@ import sqlite3
 import pandas as pd
 from sentistrength import PySentiStr
 import datahandling
-import subprocess
+import matplotlib.pyplot as plt
 from scipy import stats
+from scipy.stats import kurtosis
 
 
 
@@ -39,52 +40,68 @@ def store_sent_score(csv_filepath, db_table, db):
 # correlation of the overall sentiment score of each review with the user’s rating
 def correlation_coefficient(csv_filepath, db_table, db):
     scores = datahandling.fetch_data(db_table,db)
-
     overall_sentiment_score = [score[2] for score in scores]
-
     df = pd.read_csv(csv_filepath, encoding="ISO-8859-1")
     user_review_rating = df['Review Rating'].tolist()
-
-
     correlation_coefficient = stats.pearsonr(overall_sentiment_score, user_review_rating)
-    print(correlation_coefficient)
-
     return correlation_coefficient
 
-def group_reviews_by_hotel(csv_filepath):
+def group_reviews_by_hotel_and_calculate_mean_standard_deviation_and_kurtosis(csv_filepath):
     df = pd.read_csv(csv_filepath, encoding = "ISO-8859-1")
-    # Group reviews by the hotel name and collect reviews in a list
-    grouped_reviews = df.groupby('Property Name')['Review Text'].apply(list)
-    sentiment_scores = {}
-    for hotel_name, reviews in grouped_reviews.items():
-        # Apply sentiment analysis to the list of reviews for each hotel
-        sentiment_scores[hotel_name] = analyze_sentiment(reviews)
+    grouped = df.groupby('Property Name')['Review Rating']
+    results = grouped.agg(['mean', 'std', kurtosis]).reset_index()
+    # Access and print the mean and standard deviation for each hotel
+    for index, row in results.iterrows():
+        hotel_name = row['Property Name']
+        mean = row['mean']
+        std = row['std']
+        kurt = row['kurtosis']
+
+        print(f'Hotel: {hotel_name}, Mean: {mean}, Std: {std}, Kurtosis: {kurt}')
+
+    # Set a threshold to distinguish low and high standard deviations
+    std_deviation_threshold = 1.0  # You can adjust this threshold as needed
+
+    # Identify hotels with low and high standard deviations
+    low_std_deviation_hotels = results[results['std'] < std_deviation_threshold]
+    high_std_deviation_hotels = results[results['std'] >= std_deviation_threshold]
+    print("Hotels with Low Standard Deviation:")
+    print(low_std_deviation_hotels)
+    print("\nHotels with High Standard Deviation:")
+    print(high_std_deviation_hotels)
+
+    # Comment whether the high variation of standard deviation occurs in expensive hotel or cheap hotels
+    print("Hotels with Low Standard Deviation tend to have relatively consistent ratings.")
+    print("Hotels with High Standard Deviation tend to have more variable ratings.")
 
 
+def construct_histogram_for_star_categories(csv_filepath):
+    df = pd.read_csv(csv_filepath, encoding="ISO-8859-1")
 
-# This function should return sentiment scores
-def analyze_sentiment(reviews):
-    sentiment_scores = []
+    # list to store the proportion of hotels exceeding the threshold for each review rating
+    review_ratings = df['Review Rating'].unique()
+    proportions = []
+    std_deviation_threshold = 0.5 # You can adjust this threshold as needed
 
-    # Path to the Sentistrength JAR file and the SentiStrength data folder
-    sentistrength_jar = 'F:/GitStuff/Sentiment-Analyzer/SentiStrength/SentiStrengthCom.jar'
-    sentistrength_data = 'F:/GitStuff/Sentiment-Analyzer/SentiStrength/SentStrength_Data_Sept2011/'
+    for rating in review_ratings:
+        subset = df[df['Review Rating'] == rating]
+        print('subset',subset)
+        std_deviation_subset = subset.groupby('Property Name')['Review Rating'].std()
+        proportion = (std_deviation_subset > std_deviation_threshold).mean()
+        proportions.append(proportion)
 
-    for review in reviews:
-        # we have to format the text appropriately for Sentistrength (e.g., remove special characters)
-        # Then, call Sentistrength using subprocess
-        command = ['java', '-jar', sentistrength_jar, 'sentidata', sentistrength_data, 'text', review]
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Extract the sentiment score from the result
-        sentiment_score = int(result.stdout.strip())
-
-        sentiment_scores.append(sentiment_score)
-
-    return sentiment_scores
-
+    # Create a histogram to visualize proportions for each review rating
+    plt.bar(review_ratings, proportions)
+    plt.xlabel("Review Rating")
+    plt.ylabel("Proportion of Hotels with High Standard Deviation")
+    plt.title("Proportion of Hotels with High Standard Deviation by Review Rating")
+    plt.show()
 
 
 if __name__ == '__main__':
     # store_sent_score('data/London_hotel_reviews.csv', 'raw_sentiment_scores', 'raw_sentiment_scores.db') # I used this line to calculate and store all of the sentiment scores into raw_sentiment_scores.db database.
-    pass
+    correlation_coefficient('data/London_hotel_reviews.csv', 'raw_sentiment_scores', 'raw_sentiment_scores.db')
+    group_reviews_by_hotel_and_calculate_mean_standard_deviation_and_kurtosis('data/London_hotel_reviews.csv')
+    construct_histogram_for_star_categories('data/London_hotel_reviews.csv')
+
+
