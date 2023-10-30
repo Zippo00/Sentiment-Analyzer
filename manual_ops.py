477e8beceb080433b318a795908c6f069ab6bb16
Mikko Lempinen <101577117+Zippo00@users.noreply.github.com>
2023-10-30 14:31:45 +0200

    Update manual_ops.py

--- manual_ops.py
+++ manual_ops.py
@@ -3,27 +3,9 @@ For managing the database etc.
 
 Write the script you wish to execute after line: "if __name__ == _main_", save the file and execute via terminal.
 '''
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-
 import datahandling
 import backend_functions
 
-
-def preprocess_text(text):
-    # Tokenize the text
-    words = word_tokenize(text)
-
-    # Remove punctuation and convert to lowercase
-    words = [word.lower() for word in words if word.isalpha()]
-
-    # Remove stopwords
-    words = [word for word in words if word not in stopwords.words('english')]
-
-    # Join the words back into a clean text
-    cleaned_text = ' '.join(words)
-
-    return cleaned_text
 
 if __name__ == '__main__':
     #datahandling.sql_execute("CREATE TABLE raw_sentiment_scores (pos_score INTEGER, neg_score INTEGER, overall_score INTEGER)", "raw_sentiment_scores.db") # I used this for creating the existing raw_sentiment_scores.db file -Mikko
